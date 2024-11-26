915dd9f9384e784d3cf8b1ee272f3a0741827283
Snehal Saurabh <snehalsaurabh.code@gmail.com>
2024-11-27 02:57:54 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "PoultryGuard"
+AUTHOR_USER_NAME = "snehalsaurabh"
+SRC_REPO = "PoultryGuard" 
+AUTHOR_EMAIL = "snehalsaurabh.code@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="PoultryGuard is a smart tool designed to detect coccidiosis in poultry, enhancing health monitoring and ensuring early intervention for healthier flock.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
